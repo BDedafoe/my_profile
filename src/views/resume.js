8cecbdb203c9a42ad86cd52f52e157011106f432
BDedafoe <bmd6260@gmail.com>
2022-10-21 19:45:17 -0500

    added resume

--- src/views/resume.js
+++ src/views/resume.js
@@ -2,10 +2,49 @@ import React from 'react';
 
 const Resume = () => { 
   return (
-    <div>
-      <h3>Resume</h3>
-      <div>
-        
+    <div className="resumeContainer">
+      <div className="resumeLeft">
+        <h2>Skills</h2>
+          <li>Back-End Development</li>
+          <li>UX and UI Design</li>
+          <li>Front-End Development</li>
+          <li>HTML and Web Accessibility</li>
+          <li>JavaScript | React | Redux | Express</li>
+          <li>Node.js</li>
+          <li>CSS and CSS Frameworks</li>
+          <li>SQL | Sequelize | PGAdmin | Postgres | MongoDB</li>
+          <li>Database Development</li>
+          <li>Github</li>
+        <br></br>
+          <h2>Certification</h2>
+            <h3>Google Data Analytics Specialization</h3>
+            <h3>Coursera | Google - September 2022</h3>
+          <h2>Education</h2>
+            <h3>NC State Coding Bootcamp</h3>
+            <h3>North Carolina State University - Raleigh, NC</h3>
+            <h3>December 2022</h3>
+          <br></br>
+            <h3>Bachelor of Science</h3>
+            <h3>Computer Information Systems</h3>
+            <h3>University of Mount Olive</h3>
+            <h3>Mount Olive, NC December 2012</h3>
+      </div>
+      <div className="resumeRight">
+          <h2>Summary</h2>
+            <h3>Accomplished COO looking to channel my skillsets toward a new career in programming.  Experience in full stack development, and a passion for learning new skills and developing new applications. I’m decisive, make quick and effective decisions, and maintain excellent client relations while meeting deadlines.  I work extremely hard and will go above and beyond to meet more than the expectation. </h3>
+        <br></br>
+          <h2>Work History</h2>
+          <h2>Iron Box</h2>
+          <h2>COO | Raleigh, North Carolina</h2>
+          <h2>April 2013 - March 2022</h2>
+            <li>Managed revenue models, process flows, operations, and customer deadlines</li>
+            <li>Implemented policies to promote company culture and increase productivity across all departments  </li>
+            <li>Reduced costs through client and distributor price negotiations by 24%</li>
+            <li>Expanded company workforce to keep up with increased demand by 60%</li>
+            <li>Created customer database, vendor cost analysis reports, project timelines, and inventory projections</li>
+            <li>Developed non-disclosure agreements to work with competitors </li>
+            <li>Assisted in the development of employee handbooks and company procedures for each department</li>
+            <li>Resolved problems with high-profile customers to solidify customer retention</li>
       </div>
     </div>
   );
