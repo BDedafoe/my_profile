b17ebf14d016b162687b5f2be3e3953cc5c87f98
BDedafoe <bmd6260@gmail.com>
2022-10-27 06:45:49 -0500

    downgraded react to v17

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
-import { App } from './App';
+import {App} from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="/home"/>);
+const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
