452245873afc8baa7f3f4a87bb45388fa4d09f6f
BDedafoe <bmd6260@gmail.com>
2022-11-07 20:15:34 -0600

    added petfax app for portfolio

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = document.getElementById('root'); // <- This is the //correct method call for React version 17
 render(
   <React.StrictMode>
-    <App tab="/home"/>
+    <App />
   </React.StrictMode>,
   root
 );
